1260dd97c5762219f8202cec37897a70f8c87de4
Mike <adadminmin@MacGyver.home>
2016-03-01 00:57:05 -0500

    accommodate different package names and version specs in deb vs rpm distros

--- scripts/configureCassandra.sh
+++ scripts/configureCassandra.sh
@@ -2,13 +2,12 @@
 
 ctx logger info "configure Cassandra BEGIN"
 
-packages="cassandra21-tools datastax-agent dsc21 opscenter"
-
 case $PLATFORM in
 	deb)
 		repoFile=/etc/apt/sources.list.d/cassandra.sources.list
 		[ -e $repoFile ] || echo "deb http://debian.datastax.com/community stable main" >> $repoFile
 		curl -L https://debian.datastax.com/debian/repo_key | apt-key add -
+		packages="cassandra-tools=2.1.13 datastax-agent dsc21 opscenter cassandra=2.1.13"
 		java -version 2>&1 | egrep '1.[78]' > /dev/null || packages="openjdk-7-jre-headless $packages"
 		apt-get update && apt-get install $packages
 		;;
@@ -21,6 +20,7 @@ case $PLATFORM in
 			enabled = 1
 			gpgcheck = 0
 			EOF
+		packages="cassandra21-tools datastax-agent dsc21 opscenter"
 		java -version 2>&1 | egrep '1.[78]' > /dev/null || packages="java-1.7.0-openjdk-headless $packages"
 		yum -y install $packages
 		chkconfig --add cassandra
